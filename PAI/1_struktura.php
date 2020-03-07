eb6d3b679d3cfe832de608ad928e536ea0e2757c
markowski-krystian <krystianmarkowski1998@wp.pl>
2020-03-07 11:06:48 +0100

    first_file

---
+++ PAI/1_struktura.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+
+<html lang = "pl" dir = "ltr">
+
+    <head>
+        <meta charset = "UTF-8">
+        <title>Struktura dokumentu</title>
+    </head>
+
+    <body>
+        <?php
+            echo "test";
+        ?>
+    </body>
+
+</html>
