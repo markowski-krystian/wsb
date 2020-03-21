53627e42be4e23a0f970eacb5513ebf15e974f68
markowski-krystian <krystianmarkowski1998@wp.pl>
2020-03-21 14:40:43 +0100

    utworzenie tabeli i pliku css

--- PAI/e2.php
+++ PAI/e2.php
@@ -3,10 +3,30 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Tabela</title>
+    <link rel="stylesheet" href="e2.css">
 </head>
 <body>
     <table>
+        <tbody>
+        <tr>
+            <td rowspan="2">1.1</td>
+            <td>1.2</td>
+        </tr>
+        
+        <tr>
+            <td rowspan="2">2.2</td>
+        </tr>
+        
+        <tr>
+            <td>31.</td>
+        </tr>
+
+        <tr>
+            <td colspan="2">4.1</td>
+        </tr>
+    </tbody>
     </table>
+
 </body>
 </html>
