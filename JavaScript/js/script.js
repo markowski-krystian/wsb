68bb9971596a29af18deded421794c11e5b331c3
markowski-krystian <61902821+markowski-krystian@users.noreply.github.com>
2021-06-18 20:07:47 +0200

    Poprawka do funkcji pobierającej dane pogodowe

--- JavaScript/js/script.js
+++ JavaScript/js/script.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+jQuery(document).ready(function() {
     //Definiowanie dni polskich w tablicy
     let days = ["Niedziela", "Poniedziałek", "Wtorek", "Środa", "Czwartek", "Piątek", "Sobota"];
     //Pobranie już zdefiniowanych miesięcy
@@ -9,7 +9,7 @@ $(document).ready(function() {
     });
     let myDate = new Date();
     //Wyświetlenie daty
-    $("#date").html(days[myDate.getDay()]+ ', ' + format.format(myDate));
+    jQuery("#date").html(days[myDate.getDay()]+ ', ' + format.format(myDate));
     //Definicja godziny
     function time(){
         let now = new Date();
@@ -17,26 +17,27 @@ $(document).ready(function() {
     }
     //Wyświetlenie godziny
     setInterval(function(){
-        $("#time").html(time());
+        jQuery("#time").html(time());
     }, 1000);
 
-    //Klucz API
-    const API_KEY = 'ec3e7b997ab027b3c24bb0a847364561';
-    let ENDPOINT = 'https://api.openweathermap.org/data/2.5/forecast?q='+ $('#input_localization').val() +'&lang=pl&appid=' + API_KEY;
+
     //Dzienna
-    $('#search_button').click(function(){
-        $.ajax({
+    jQuery('#search_button').click(function(){
+		    //Klucz API
+		const API_KEY = 'ec3e7b997ab027b3c24bb0a847364561';
+		let ENDPOINT = 'https://api.openweathermap.org/data/2.5/forecast?q='+ $('#input_localization').val() +'&lang=pl&appid=' + API_KEY;
+        jQuery.ajax({
             url:ENDPOINT,
             type:'GET',
             success:function(result){
-                $("#weather_result").html('');
-                    var n = $(document).height();
-                    $('html, body').animate({ scrollTop: n }, 2000);
-                $.each(result.list, function(index, value){
+                jQuery("#weather_result").html('');
+                    var n = jQuery(document).height();
+                    jQuery('html, body').animate({ scrollTop: n }, 2000);
+                jQuery.each(result.list, function(index, value){
                     if (index<=8){
                         let s = value.dt_txt;
                         let d = new Date(s);
-                        $('#weather_result').append(`
+                        jQuery('#weather_result').append(`
                         <div class="weather_data">
                             <div class="day"><p>` + d.toLocaleTimeString() + `</p></div>
                             <div class="temperature"><p>` + (value.main.temp - 273.15).toFixed(2) + ` °</p></div>
@@ -52,25 +53,29 @@ $(document).ready(function() {
                 });
             },
             error: function(){
-                $("#error").delay(3000).fadeIn(800);
-                $("#error_city").text('Nieprawidłowa nazwa miasta!');
-                $("#error").delay(3000).fadeOut(800); 
+                jQuery("#error").delay(3000).fadeIn(800);
+                jQuery("#error_city").text('Nieprawidłowa nazwa miasta!');
+                jQuery("#error").delay(3000).fadeOut(800); 
               }
         });
     });
-    $('#search_button_monthly').click(function(){
-        $.ajax({
+    jQuery('#search_button_monthly').click(function(){
+		    //Klucz API
+		const API_KEY = 'ec3e7b997ab027b3c24bb0a847364561';
+		let ENDPOINT = 'https://api.openweathermap.org/data/2.5/forecast?q='+ $('#input_localization').val() +'&lang=pl&appid=' + API_KEY;
+		console.log(jQuery("#input_localization").val());
+        jQuery.ajax({
             url:ENDPOINT,
             type:'GET',
             success:function(result){
-                $("#weather_result").html('');
-                    var n = $(document).height();
-                    $('html, body').animate({ scrollTop: n }, 2000);
-                $.each(result.list, function(index, value){
+                jQuery("#weather_result").html('');
+                    var n = jQuery(document).height();
+                    jQuery('html, body').animate({ scrollTop: n }, 2000);
+                jQuery.each(result.list, function(index, value){
                     if(index==0 || index==8 || index==16 || index==24 || index==32){
                         let s = value.dt_txt;
                         let d = new Date(s);
-                        $('#weather_result').append(`
+                        jQuery('#weather_result').append(`
                         <div class="weather_data">
                             <div class="day"><p>` + d.toLocaleDateString() + `</p></div>
                             <div class="temperature"><p>` + (value.main.temp - 273.15).toFixed(2) + ` °</p></div>
@@ -86,9 +91,9 @@ $(document).ready(function() {
                 });
             },
             error: function(){
-                $("#error").delay(3000).fadeIn(800);
-                $("#error_city").text('Nieprawidłowa nazwa miasta!');
-                $("#error").delay(3000).fadeOut(800); 
+                jQuery("#error").delay(3000).fadeIn(800);
+                jQuery("#error_city").text('Nieprawidłowa nazwa miasta!');
+                jQuery("#error").delay(3000).fadeOut(800); 
               }
         });
     });
